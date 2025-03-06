f70b97660050d41fe068c6bc6467c67f0b5ffab0
Yaroslav Balan <balanyaroslaw@gmail.com>
2025-03-06 10:48:56 +0200

    add repo link

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -58,7 +58,7 @@ function Dashboard() {
             <footer className={`${deviceType===Device.DESKTOP?'fixed':''} bottom-0 left-0 w-full bg-gray-800 text-white py-4`}>
                 <div className="container mx-auto flex flex-col md:flex-row items-center justify-between px-4">
                     <div className="flex space-x-4 mt-2 md:mt-0">
-                        <a href="https://github.com/balanyaroslaw/booksTestTask" className="hover:underline">My Github</a>
+                        <a href="https://github.com/balanyaroslaw/booksTestTask"  target="_blank" className="hover:underline">My Github</a>
                     </div>
                 </div>
             </footer>
